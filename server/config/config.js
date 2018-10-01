66768d343e178e20dc78c6a30783c134adb98aeb
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-10-01 10:33:16 -0700

    adding database

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,14 @@
 const config = {
-    production:{
-        SECRET: process.env.SECRET,
-        DATABASE: process.env.MONGODB_URI
-    },
-    default:{
-        SECRET: 'SUPERSECRETPASSWORD123',
-        DATABASE: 'mongodb://localhost:27017/booksShelf'
-    }
-}
+  production: {
+    SECRET: process.env.SECRET,
+    DATABASE: 'mongodb://user1:password1@ds119343.mlab.com:19343/book_shelf',
+  },
+  default: {
+    SECRET: 'SUPERSECRETPASSWORD123',
+    DATABASE: 'mongodb://localhost:27017/booksShelf',
+  },
+};
 
-exports.get = function get(env){
-    return config[env] || config.default
-} 
+exports.get = function get(env) {
+  return config[env] || config.default;
+};
